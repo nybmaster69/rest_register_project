0877d202b99a4934b238c693714ea9de1b43c467
nybmaster69 <isa.bek1945@gmail.com>
2022-02-22 20:10:06 +0600

    Add login logic

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,8 +1,11 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.views import APIView
 
 from rest_framework.response import Response
 from rest_framework import status
-from applications.account.serializers import RegisterSerializer
+from applications.account.serializers import RegisterSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class RegistrationView(APIView):
@@ -13,3 +16,15 @@ class RegistrationView(APIView):
             serializer.save()
             return Response('Successfully signed up', status=status.HTTP_201_CREATED)
 
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request):
+        user = request.user
+        Token.objects.filter(user=user).delete()
+        return Response('Successfully logged out', status=status.HTTP_200_OK)
